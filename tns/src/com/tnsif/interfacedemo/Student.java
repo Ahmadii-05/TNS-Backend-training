26b769fd011ac898205718b5164b9310c8d81b0d
Ahmadi <ahmadi3055.am@gmail.com>
2025-05-07 23:37:50 +0530

    user input

---
+++ tns/src/com/tnsif/interfacedemo/Student.java
@@ -0,0 +1,30 @@
+package com.tnsif.interfacedemo;
+//take user input
+import java.util.Scanner;
+
+public class Student {
+	String username1;
+	int id;
+	int number;
+
+	// Constructor to input data
+    Student(String String) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the username: ");
+        username1 = sc.nextLine();
+
+        System.out.print("Enter the ID: ");
+        id = sc.nextInt();
+
+        System.out.print("Enter the number: ");
+        number = sc.nextInt();
+    }
+
+    void display() {
+        System.out.println("Username: " + username1);
+        System.out.println("ID: " + id);
+        System.out.println("Number: " + number);
+    }
+	
+}
---
+++ tns/src/com/tnsif/interfacedemo/StudentMain.java
@@ -0,0 +1,10 @@
+package com.tnsif.interfacedemo;
+
+public class StudentMain {
+
+	public static void main(String[] args) {
+		Student s1= new Student();
+		s1.display();
+	}
+
+}
