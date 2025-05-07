707257d579d033b3d93327b27d7d8e236f29b2dd
Ahmadi <ahmadi3055.am@gmail.com>
2025-05-07 23:33:40 +0530

    Abstraction

---
+++ tns/src/com/tnsif/oopsconsept/Abstract.java
@@ -0,0 +1,17 @@
+package com.tnsif.oopsconsept;
+//abstract class
+public abstract class Abstract {
+
+	    // Abstract method
+	    abstract void draw();
+
+	    // Regular method
+	    void display() {
+	        System.out.println("This is a shape.");
+	    }
+	}
+
+	
+
+	
+
---
+++ tns/src/com/tnsif/oopsconsept/AbstractMain.java
@@ -0,0 +1,15 @@
+package com.tnsif.oopsconsept;
+
+public class AbstractMain {
+
+	public static void main(String[] args) {
+		 Abstract s1 = new Circle();
+	        
+
+	        s1.display();   // Output: This is a shape.
+	        s1.draw();      // Output: Drawing a circle.
+		
+
+	}
+
+}
