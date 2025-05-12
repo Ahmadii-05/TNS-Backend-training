0e34f9dde7bf88bc35b1c9c1ff8e3afe81a60ebe
Ahmadi <ahmadi3055.am@gmail.com>
2025-05-12 18:48:19 +0530

    linked list

---
+++ tns/src/com/tnsif/collectiondemo/LinkedListDemo.java
@@ -0,0 +1,27 @@
+package com.tnsif.collectiondemo;
+//allow duplications & preserve insertion order
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListDemo {
+	 public static void main(String[] args) {
+	        List<Integer> l = new LinkedList<Integer>();
+	        l.add(10);
+	        l.add(10);
+	        l.add(34);
+	        l.add(23);
+
+	        for (int i : l) {
+	            System.out.println(i);
+	        }
+
+	        l.remove(1); // Removes the element at index 1 (second 10)
+	        System.out.println("After deletion");
+
+	        for (int i : l) {
+	            System.out.println(i);
+	        }
+	    }
+	}
+
+
